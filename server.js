9fa68e679e16f6db5978de1d4095fb9deb317006
jorg3lopez <49923512+jorg3lopez@users.noreply.github.com>
2020-04-15 14:58:59 -0700

    Added middleware and dependencies to the server
    Added ejs, morgan, express-session, body-parser.
    
    Added bootstrap to index.html
    
    note: since ejs was added, the html files need their extensions changed to '.ejs', eg: index.ejs
    
    the path to '/views' folder is set so when using res.render, we only need to specify the file inside the folder. egres.render('./index.ejs');:

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const express = require("express");
+const ejs = require('ejs');
 const app = express();
 const path = require("path");
-const port = 9000;
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const port = process.env.PORT || 9000;
+
+// Setting up the requests and responses
+app.use(session({   //<------- In case we have to use a database
+  secret: 'NASA',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+// Middleware
+app.use(morgan('dev'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: false}));
+
 
 // viewed at http://localhost:8080
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/views/index.html"));
+  // res.sendFile(path.join(__dirname + "/views/index.html"));
+  res.render('./index.ejs');
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
