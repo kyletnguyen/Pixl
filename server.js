c4a23ebd68fa8631ce230e8087334553bca768ec
kyletnguyen <kyletnguyen43@gmail.com>
2020-04-17 15:07:21 -0700

    Added .csv reader and separated by detector a and b.  Added d3 heatmap elements (title, axis titles, tooltip)

--- server.js
+++ server.js
@@ -3,11 +3,55 @@ const app = express();
 const path = require("path");
 const port = 9000;
 
+const csv = require("csv-parser");
+const fs = require("fs");
+const qev_csv = [];
+const detA_csv = [];
+const detB_csv = [];
+const filename = path.normalize(__dirname + "/References/quantified_element_values.csv");
+
+var model = {
+  element: "",
+  update: function (e) {
+    this.element = e;
+  },
+};
+
 app.use(express.static(__dirname));
 
-// viewed at http://localhost:8080
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/views/index.html"));
 });
+
+app.get("/mg", function (req, res) {});
+app.get("/al", function (req, res) {});
+app.get("/ca", function (req, res) {});
+app.get("/ti", function (req, res) {});
+app.get("/fe", function (req, res) {});
+app.get("/si", function (req, res) {});
+
+fs.createReadStream(filename)
+  .on("error", () => {
+    console.log("Error reading CSV file");
+  })
+  //PMC,Detector,Mg_%,Al_%,Ca_%,Ti_%,Fe_%,Si_%,Mg_int,Al_int,Ca_int,Ti_int,Fe_int,Si_int,image_i,image_j
+  .pipe(csv())
+  .on("data", (row) => {
+    qev_csv.push(row);
+  })
+  .on("end", () => {
+    console.log("Finished CSV file");
+    separateDetectors(qev_csv);
+  });
+
+function separateDetectors() {
+  for (var i = 0; i < qev_csv.length; i++) {
+    if (qev_csv[i].Detector === "A") {
+      detA_csv.push(qev_csv[i]);
+    } else {
+      detB_csv.push(qev_csv[i]);
+    }
+  }
+}
 
 app.listen(port, () => console.log(`Hosting web application on port ${port}`));
