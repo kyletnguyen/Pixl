5d2e054aaa1a75b09441ed05201d465aa0621e09
kyletnguyen <kyletnguyen43@gmail.com>
2020-04-27 19:17:28 -0700

    Removed parts from server.js

--- server.js
+++ server.js
@@ -3,48 +3,6 @@ const app = express();
 const path = require("path");
 const port = 9000;
 
-const csv = require("csv-parser");
-const fs = require("fs");
-
-const createCsvWriter = require("csv-writer").createObjectCsvWriter;
-const detA_csv = path.normalize(__dirname + "/References/detA.csv");
-
-const detB_csv = path.normalize(__dirname + "/References/detB.csv");
-
-const csvWriterA = createCsvWriter({
-  path: detA_csv,
-  header:
-    //PMC,Detector,Mg_%,Al_%,Ca_%,Ti_%,Fe_%,Si_%,Mg_int,Al_int,Ca_int,Ti_int,Fe_int,Si_int,image_i,image_j
-    [
-      { id: "PMC", title: "PMC" },
-      { id: "Mg_%", title: "Mg" },
-      { id: "Al_%", title: "Al" },
-      { id: "Ca_%", title: "Ca" },
-      { id: "Ti_%", title: "Ti" },
-      { id: "Fe_%", title: "Fe" },
-      { id: "Si_%", title: "Si" },
-      { id: "image_i", title: "image_i" },
-      { id: "image_j", title: "image_j" },
-    ],
-});
-
-const csvWriterB = createCsvWriter({
-  path: detB_csv,
-  header:
-    //PMC,Detector,Mg_%,Al_%,Ca_%,Ti_%,Fe_%,Si_%,Mg_int,Al_int,Ca_int,Ti_int,Fe_int,Si_int,image_i,image_j
-    [
-      { id: "PMC", title: "PMC" },
-      { id: "Mg_%", title: "Mg" },
-      { id: "Al_%", title: "Al" },
-      { id: "Ca_%", title: "Ca" },
-      { id: "Ti_%", title: "Ti" },
-      { id: "Fe_%", title: "Fe" },
-      { id: "Si_%", title: "Si" },
-      { id: "image_i", title: "image_i" },
-      { id: "image_j", title: "image_j" },
-    ],
-});
-
 const qev_csv = [];
 const detA = [];
 const detB = [];
@@ -186,39 +144,5 @@ app.get("/si", function (req, res) {
   response = res;
   model.update("Si");
 });
-
-// fs.createReadStream(filename)
-//   .on("error", () => {
-//     console.log("Error reading CSV file");
-//   })
-//   //PMC,Detector,Mg_%,Al_%,Ca_%,Ti_%,Fe_%,Si_%,Mg_int,Al_int,Ca_int,Ti_int,Fe_int,Si_int,image_i,image_j
-//   .pipe(csv())
-//   .on("data", (row) => {
-//     qev_csv.push(row);
-//   })
-//   .on("end", () => {
-//     console.log("Finished CSV file");
-//     separateDetectors(qev_csv);
-//   });
-
-// function separateDetectors() {
-//   for (var i = 0; i < qev_csv.length; i++) {
-//     if (qev_csv[i].Detector === "A") {
-//       detA.push(qev_csv[i]);
-//     } else {
-//       detB.push(qev_csv[i]);
-//     }
-//   }
-//   csvWriterA
-//     .writeRecords(detA) // returns a promise
-//     .then(() => {
-//       console.log("Wrote CSV file for Detector A");
-//     });
-//   csvWriterB
-//     .writeRecords(detB) // returns a promise
-//     .then(() => {
-//       console.log("Wrote CSV file for Detector B");
-//     });
-// }
 
 app.listen(port, () => console.log(`Hosting web application on port ${port}`));
