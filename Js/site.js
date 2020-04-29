1ebea433c30d0d55d8f2dd3fd01a0325a748e26b
jorg3lopez <49923512+jorg3lopez@users.noreply.github.com>
2020-04-29 01:42:24 -0700

    Added handlebars template
    Added handlebars as our view engine. Because of this, some changes in the file structure and file extensions were made. 
    Please add: npm install express-handlebars 
    to the dependencies.
    - layouts folder was added to include layouts, so far we only have one layout.
    - index.html remained unchanged.
    - element.html was replaced by index.hbs located in views/layouts folder.
    - site.css remained unchanged.
    - sites.js, the first jQuery input "$('body').is('.element-page') does not execute. This blocks 'graphDetector()' from being called, thus not pulling the element name that the user requested. To bypass this, I called 'graphDetector()' from outside the jQuery statement. We probably need to verify if this causes any bugs. So far, it didn't when I ran the server.
    -added main.hbs to the views folder. This file receives the element from the 'GET' request (model.update()) and passes it to the index.hbs file.
    - server.js's code changed from passing the html variable to rendering the index.hbs dynamically. All old code was commented out, so feel free to delete it once we test the code and it runs smoothly.
    
    -----side note-----
    - site.js has a few console.log() lines which I used for debugging. I forgot to delete them or comment them out.

--- Js/site.js
+++ Js/site.js
@@ -1,53 +1,436 @@
-// set the dimensions and margins of the graph
-var margin = { top: 30, right: 30, bottom: 30, left: 30 },
-  width = 450 - margin.left - margin.right,
-  height = 450 - margin.top - margin.bottom;
+$(function () {
+  if ($("body").is(".element-page")) {
+    console.log('inside body')
+    graphDetector();
+  }
+  graphDetector();
+console.log('inside site.js')
+  function graphDetector() {
+    var element = $("#elementId").val();
+    console.log(element)
+    var color = $("#colorId").val();
 
-// append the svg object to the body of the page
-var svg = d3
-  .select("#my_dataviz")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var elementName;
+    var elementPerRef;
 
-// Labels of row and columns
-var myGroups = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
-var myVars = ["v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10"];
+    // var csvRef = $("input[name='detector-select']:checked").val() === "A" ? "References/detA.csv" : "References/detB.csv";
+    var csvRef = "/References/quantified_element_values.csv";
+    var detector = $("input[name='detector-select']:checked").val() === "A" ? "A" : "B";
+    // set the dimensions and margins of the graph
 
-// Build X scales and axis:
-var x = d3.scaleBand().range([0, width]).domain(myGroups).padding(0.01);
-svg
-  .append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x));
+    $("svg").remove();
+    $("#legend").empty();
 
-// Build X scales and axis:
-var y = d3.scaleBand().range([height, 0]).domain(myVars).padding(0.01);
-svg.append("g").call(d3.axisLeft(y));
+    var margin = { top: 140, right: 73, bottom: 50, left: 75 },
+      width = 900 - margin.left - margin.right,
+      height = 770 - margin.top - margin.bottom;
 
-// Build color scale
-var myColor = d3.scaleLinear().range(["white", "#69b3a2"]).domain([1, 100]);
+    // append the svg object to the body of the page
+    var svg = d3
+      .select("#detector")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .call(
+        d3
+          .zoom()
+          .extent([
+            [0, 0],
+            [width, height],
+          ])
+          .scaleExtent([1, 8])
+          .on("zoom", zoomed)
+      )
+      // .call(
+      //   d3.zoom().on("zoom", function () {
+      //     svg.attr("transform", d3.event.transform);
+      //   })
+      // )
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-//Read the data
-d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/heatmap_data.csv", function (data) {
-  svg
-    .selectAll()
-    .data(data, function (d) {
-      return d.group + ":" + d.variable;
-    })
-    .enter()
-    .append("rect")
-    .attr("x", function (d) {
-      return x(d.group);
-    })
-    .attr("y", function (d) {
-      return y(d.variable);
-    })
-    .attr("width", x.bandwidth())
-    .attr("height", y.bandwidth())
-    .style("fill", function (d) {
-      return myColor(d.value);
+    function zoomed() {
+      svg.attr("transform", d3.event.transform);
+    }
+
+    svg.append("image").attr("xlink:href", "/References/pixl_original.jpg").attr("class", "pixl-img");
+
+    //Read the data
+    ///References/detector.csv
+    d3.csv(csvRef, function (data) {
+      var filterData = data.filter(function (e) {
+        return e.Detector == detector;
+      });
+
+      //PMC,Mg,Al,Ca,Ti,Fe,Si,image_i,image_j
+      var minX = Number.MAX_SAFE_INTEGER;
+      var maxX = 0;
+      var minY = Number.MAX_SAFE_INTEGER;
+      var maxY = 0;
+      var minPer = Number.MAX_SAFE_INTEGER;
+      var maxPer = 0;
+
+      for (var i = 0; i < filterData.length; i++) {
+        switch (element) {
+          case "Mg":
+            elementPerRef = Number(filterData[i]["Mg_%"]);
+            break;
+          case "Al":
+            elementPerRef = Number(filterData[i]["Al_%"]);
+            break;
+          case "Ca":
+            elementPerRef = Number(filterData[i]["Ca_%"]);
+            break;
+          case "Ti":
+            elementPerRef = Number(filterData[i]["Ti_%"]);
+            break;
+          case "Fe":
+            elementPerRef = Number(filterData[i]["Fe_%"]);
+            break;
+          case "Si":
+            elementPerRef = Number(filterData[i]["Si_%"]);
+            break;
+        }
+        if (elementPerRef < minPer) {
+          minPer = elementPerRef;
+        }
+
+        if (elementPerRef > maxPer) {
+          maxPer = elementPerRef;
+        }
+
+        if (Number(filterData[i].image_i) < minX) {
+          minX = Number(filterData[i].image_i);
+        }
+
+        if (Number(filterData[i].image_i) > maxX) {
+          maxX = Number(filterData[i].image_i);
+        }
+
+        if (Number(filterData[i].image_j) < minY) {
+          minY = Number(filterData[i].image_j);
+        }
+        if (filterData[i].image_j > maxY) {
+          maxY = Number(filterData[i].image_j);
+        }
+      }
+
+      // var myImage_i = new Array(Math.ceil(maxX));
+      // var myImage_j = new Array(Math.ceil(maxY));
+
+      var myImage_i = new Array(580);
+      var myImage_j = new Array(752);
+      minX = 0;
+      minY = 0;
+
+      for (var i = 0; i <= 580; i++) {
+        myImage_i.push(i);
+      }
+
+      for (var j = 0; j <= 752; j++) {
+        myImage_j.push(j);
+      }
+
+      // Build X scales and axis:
+      var x = d3.scaleBand().range([0, width]).domain(myImage_i).padding(0.05);
+      var xAxis = d3
+        .axisBottom(x)
+        .tickSize(5)
+        .tickSizeOuter(0)
+        .tickValues(
+          x.domain().filter(function (d, i) {
+            if (i === 581) {
+              return i;
+            } else {
+              return !((i - 1) % 50);
+            }
+          })
+        );
+      svg
+        .append("g")
+        .call(xAxis)
+        .style("font-size", 15)
+        .attr("transform", "translate(0," + height + ")")
+        .select(".domain");
+
+      // text label for the x axis
+      svg
+        .append("text")
+        .attr("transform", "translate(" + width / 2 + " ," + (height + 40) + ")")
+        .style("text-anchor", "middle")
+        .attr("font-weight", "bold")
+        .text("image_i");
+
+      // Build Y scales and axis:
+      var y = d3.scaleBand().range([height, 0]).domain(myImage_j).padding(0.05);
+      var yAxis = d3
+        .axisLeft(y)
+        .tickSize(5)
+        .tickSizeOuter(0)
+        .tickValues(
+          y.domain().filter(function (d, i) {
+            if (i === 753) {
+              return i;
+            } else {
+              if (i != 751) return !((i - 1) % 50);
+            }
+          })
+        );
+
+      svg.append("g").style("font-size", 15).call(yAxis).select(".domain");
+
+      // text label for the y axis
+      svg
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 25)
+        .attr("x", 0 - height / 2)
+        .attr("dy", ".25em")
+        .style("text-anchor", "middle")
+        .attr("font-weight", "bold")
+        .text("image_j");
+
+      var elementColor;
+      switch (element) {
+        case "Mg":
+          elementColor = d3.interpolateReds;
+
+          break;
+        case "Al":
+          elementColor = d3.interpolatePuBu;
+
+          break;
+        case "Ca":
+          elementColor = d3.interpolatePuBuGn;
+
+          break;
+        case "Ti":
+          elementColor = d3.interpolateYlGnBu;
+
+          break;
+        case "Fe":
+          elementColor = d3.interpolateYlGn;
+
+          break;
+        case "Si":
+          elementColor = d3.interpolateYlOrBr;
+          break;
+      }
+      // Build color scale
+      var myColor = d3.scaleSequential().interpolator(elementColor).domain([minPer, maxPer]);
+      //[Math.round(minPer * 10) / 10, Math.round(maxPer * 10) / 10]
+
+      drawLegend("#legend", myColor);
+
+      // create a tooltip
+      var tooltip = d3
+        .select("#detector")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "2px")
+        .style("border-radius", "5px")
+        .style("padding", "5px");
+
+      // Three function that change the tooltip when user hover / move / leave a cell
+      var mouseover = function (d) {
+        tooltip.style("opacity", 1);
+        d3.select(this).style("stroke", "red").style("opacity", 1);
+      };
+      var mousemove = function (d) {
+        switch (element) {
+          case "Mg":
+            elementRef = d["Mg_%"];
+
+            break;
+          case "Al":
+            elementRef = d["Al_%"];
+
+            break;
+          case "Ca":
+            elementRef = d["Ca_%"];
+
+            break;
+          case "Ti":
+            elementRef = d["Ti_%"];
+
+            break;
+          case "Fe":
+            elementRef = d["Fe_%"];
+
+            break;
+          case "Si":
+            elementRef = d["Si_%"];
+            break;
+        }
+        tooltip
+          .html("<b>" + element + " %: </b>" + elementRef + " <br><b>Location: </b>" + d.image_i + " i : " + d.image_j + " j")
+          .style("left", d3.mouse(this)[0] + (width - 650) + "px")
+          .style("top", d3.mouse(this)[1] + (height - 475) + "px");
+      };
+      var mouseleave = function (d) {
+        tooltip.style("opacity", 0);
+        d3.select(this).style("stroke", "none").style("opacity", 0.8);
+      };
+
+      // add the squares
+
+      svg
+        .selectAll()
+        .data(filterData)
+        .enter()
+        .append("rect")
+        .attr("x", function (d) {
+          return x(Math.floor(d.image_i));
+        })
+        .attr("y", function (d) {
+          return y(Math.floor(d.image_j));
+        })
+        .attr("rx", 4)
+        .attr("ry", 4)
+        .attr("width", x.bandwidth() + 1)
+        .attr("height", y.bandwidth() + 1)
+        .style("fill", function (d) {
+          switch (element) {
+            case "Mg":
+              elementRef = d["Mg_%"];
+
+              break;
+            case "Al":
+              elementRef = d["Al_%"];
+
+              break;
+            case "Ca":
+              elementRef = d["Ca_%"];
+
+              break;
+            case "Ti":
+              elementRef = d["Ti_%"];
+
+              break;
+            case "Fe":
+              elementRef = d["Fe_%"];
+
+              break;
+            case "Si":
+              elementRef = d["Si_%"];
+              break;
+          }
+          return myColor(elementRef);
+        })
+        .style("stroke-width", 4)
+        .style("stroke", "none")
+        .style("opacity", 0.8)
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave);
     });
+
+    switch (element) {
+      case "Mg":
+        elementName = "Magnesium";
+        break;
+      case "Al":
+        elementName = "Aluminum";
+        break;
+      case "Ca":
+        elementName = "Calcium";
+        break;
+      case "Ti":
+        elementName = "Titanium";
+        break;
+      case "Fe":
+        elementName = "Iron";
+        break;
+      case "Si":
+        elementName = "Silicon";
+        break;
+    }
+    // Add title to graph
+    svg
+      .append("text")
+      .attr("x", 0)
+      .attr("y", -50)
+      .attr("text-anchor", "left")
+      .style("font-size", "22px")
+      .attr("font-weight", "bold")
+      .text("Heatmap for " + elementName + " (Detector " + detector + ")");
+
+    // Add subtitle to graph
+    // svg.append("text").attr("x", 0).attr("y", -20).attr("text-anchor", "left").style("font-size", "14px").style("fill", "grey").style("max-width", 400).text("Subtitle");
+  }
+
+  // create continuous color legend
+  function drawLegend(selector_id, colorscale) {
+    var legendheight = 350,
+      legendwidth = 100,
+      margin = { top: 10, right: 60, bottom: 10, left: 2 };
+
+    var canvas = d3
+      .select(selector_id)
+      .style("height", legendheight + "px")
+      .style("width", legendwidth + "px")
+      .style("position", "relative")
+      .append("canvas")
+      .attr("height", legendheight - margin.top - margin.bottom)
+      .attr("width", 1)
+      .style("height", legendheight - margin.top - margin.bottom + "px")
+      .style("width", legendwidth - margin.left - margin.right + "px")
+      .style("border", "1px solid #000")
+      .style("position", "absolute")
+      .style("top", margin.top + "px")
+      .style("left", margin.left + "px")
+      .node();
+
+    var ctx = canvas.getContext("2d");
+
+    var legendscale = d3
+      .scaleLinear()
+      .range([1, legendheight - margin.top - margin.bottom])
+      .domain(colorscale.domain());
+
+    // image data hackery based on http://bl.ocks.org/mbostock/048d21cf747371b11884f75ad896e5a5
+    var image = ctx.createImageData(1, legendheight);
+    d3.range(legendheight).forEach(function (i) {
+      var c = d3.rgb(colorscale(legendscale.invert(i)));
+      image.data[4 * i] = c.r;
+      image.data[4 * i + 1] = c.g;
+      image.data[4 * i + 2] = c.b;
+      image.data[4 * i + 3] = 255;
+    });
+    ctx.putImageData(image, 0, 0);
+
+    // A simpler way to do the above, but possibly slower. keep in mind the legend width is stretched because the width attr of the canvas is 1
+    // See http://stackoverflow.com/questions/4899799/whats-the-best-way-to-set-a-single-pixel-in-an-html5-canvas
+    /*
+  d3.range(legendheight).forEach(function(i) {
+    ctx.fillStyle = colorscale(legendscale.invert(i));
+    ctx.fillRect(0,i,1,1);
+  });
+  */
+
+    var legendaxis = d3.axisRight().scale(legendscale).tickSize(6).ticks(8);
+
+    var svg = d3
+      .select(selector_id)
+      .append("svg")
+      .attr("height", legendheight + "px")
+      .attr("width", legendwidth + "px")
+      .style("position", "absolute")
+      .style("left", "0px")
+      .style("top", "0px");
+
+    svg
+      .append("g")
+      .attr("class", "axis")
+      .attr("transform", "translate(" + (legendwidth - margin.left - margin.right + 3) + "," + margin.top + ")")
+      .call(legendaxis);
+  }
+
+  //Event handlers
+  $(".detector-btns").on("click", function (e) {
+    graphDetector();
+  });
 });
--- server.js
+++ server.js
@@ -1,32 +1,160 @@
 const express = require("express");
-const ejs = require('ejs');
 const app = express();
 const path = require("path");
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
 const port = process.env.PORT || 9000;
-
-// Setting up the requests and responses
-app.use(session({   //<------- In case we have to use a database
-  secret: 'NASA',
-  resave: true,
-  saveUninitialized: true
-}));
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.json());
-
-// Middleware
-app.use(morgan('dev'));
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.urlencoded({ extended: false}));
+//-------------------------------------------------------------------- handlebars
+const handlebars = require('express-handlebars');
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars({
+  layouts: `${__dirname}/views/layouts`,
+  extname: 'hbs',
+  defaultLayout: 'index',
+}))
 
 
-// viewed at http://localhost:8080
+//-------------------------------------------------------------------- end_handlebars
+
+const qev_csv = [];
+const detA = [];
+const detB = [];
+const filename = path.normalize(__dirname + "/References/quantified_element_values.csv");
+
+var response;
+
+var model = {
+  element: "",
+  update: function (e) {
+    this.element = e;
+    view.render();
+  },
+};
+
+var view = {
+  render: function () {
+    var html = "";
+
+    //response.sendFile(path.join(__dirname + "/views/element.html"));
+    // html += "<!DOCTYPE html>";
+
+    // html += '<html lang="en">';
+    // html += "  <head>";
+    // html += '    <meta charset="utf-8" />';
+
+    // html += "    <title>PIXL Heatmaps</title>";
+    // html += '    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet" />';
+    // html += '    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" />';
+    // html += '    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.0/css/bootstrap.min.css" />';
+    // html += '    <link rel="stylesheet" href="./Styles/site.css" />';
+    // html += '    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js"></script>';
+    // html += '    <script src="https://d3js.org/d3.v5.min.js"></script>';
+    // html += '    <script src="https://d3js.org/d3.v4.js"></script>';
+    // html += '    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.0/js/bootstrap.bundle.min.js"></script>';
+
+    // html += '    <script src="./Js/site.js"></script>';
+    // html += "  </head>";
+
+    // html += '  <body class="element-page">';
+    // html += "    <!-- PMC,Mg,Al,Ca,Ti,Fe,Si,image_i,image_j -->";
+    // html += '    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">';
+    // html += '      <div class="container">';
+    // html += '        <a class="navbar-brand" href="/">PIXL Heatmaps</a>';
+    // html += "        <button";
+    // html += '          class="navbar-toggler"';
+    // html += '          type="button"';
+    // html += '          data-toggle="collapse"';
+    // html += '          data-target="#navbarResponsive"';
+    // html += '          aria-controls="navbarResponsive"';
+    // html += '          aria-expanded="false"';
+    // html += '          aria-label="Toggle navigation"';
+    // html += "        >";
+    // html += '          <span class="navbar-toggler-icon"></span>';
+    // html += "        </button>";
+    // html += '        <div class="collapse navbar-collapse" id="navbarResponsive">';
+    // html += '          <ul class="navbar-nav ml-auto">';
+    // html += '            <li class="nav-item active">';
+    // html += '              <a class="nav-link" href="/">Home</a>';
+    // html += "            </li>";
+    // html += '            <li class="nav-item dropdown">';
+    // html += '              <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Select Elements</a>';
+    // html += '              <div class="dropdown-menu">';
+    // html += '                <a class="dropdown-item" href="/mg">Magnesium</a>';
+    // html += '                <a class="dropdown-item" href="/al">Aluminum</a>';
+    // html += '                <a class="dropdown-item" href="/ca">Calcium</a>';
+    // html += '                <a class="dropdown-item" href="/ti">Titanium</a>';
+    // html += '                <a class="dropdown-item" href="/fe">Iron</a>';
+    // html += '                <a class="dropdown-item" href="/si">Silicon</a>';
+    // html += "              </div>";
+    // html += "            </li>";
+    // html += "          </ul>";
+    // html += "        </div>";
+    // html += "      </div>";
+    // html += "    </nav>";
+    // html += "    <!-- Create a div where the graph will take place -->";
+    // html += ' <input type="hidden" id="elementId" name="elementId" value="' + model.element + '">';
+    // html += '    <div class="d-flex flex-row">';
+    // html += '      <div class="d-flex justify-content-start">';
+    // html += '        <div id="detector"></div>';
+    // html += "      </div>";
+
+    // html += '      <div id="legend"></div>';
+    // html += "    </div>";
+
+    // html += "    <br />";
+
+    // html += '    <div class="detector-group btn-group btn-group-toggle" data-toggle="buttons">';
+    // html += '      <label class="btn btn-light active">';
+    // html += '        <input type="radio" name="detector-select" id="detA-btn" class="detector-btns" autocomplete="off" value="A" checked="checked" /> Detector A';
+    // html += "      </label>";
+    // html +=
+    //   '      <label class="btn btn-light"> <input type="radio" name="detector-select" id="detB-btn" class="detector-btns" autocomplete="off" value="B" /> Detector B </label>';
+    // html += "    </div>";
+
+    // html += '    <!-- <button type="button" class="prev-elem btn btn-secondary btn-sm"><i class="fas fa-chevron-left"></i></button>';
+    // html += '    <button type="button" class="next-elem btn btn-secondary btn-sm"><i class="fas fa-chevron-right"></i></button> -->';
+    // html += "  </body>";
+    // html += "</html>";
+
+    // response.writeHead(200, { "Content-Type": "text/html" });
+    // response.end(html + "\n");
+    response.render('main', {layout: 'index', element: model.element});
+  },
+};
+
+app.use(express.static(__dirname));
+
 app.get("/", function (req, res) {
-  // res.sendFile(path.join(__dirname + "/views/index.html"));
-  res.render('./index.ejs');
+  res.sendFile(path.join(__dirname + "/views/index.html")); 
 });
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.get("/mg", function (req, res) {
+  response = res;
+  model.update("Mg");
+});
+
+app.get("/al", function (req, res) {
+  response = res;
+  model.update("Al");
+  // res.send("GET request for Al");
+});
+
+app.get("/ca", function (req, res) {
+  response = res;
+  model.update("Ca");
+});
+
+app.get("/ti", function (req, res) {
+  response = res;
+  model.update("Ti");
+});
+
+app.get("/fe", function (req, res) {
+  response = res;
+  model.update("Fe");
+});
+
+app.get("/si", function (req, res) {
+  response = res;
+  model.update("Si");
+});
+
+app.listen(port, () => console.log(`Hosting web application on port ${port}`));
